0dc7f27254f850010f2fcd5a6212988db281b522
Victor Arza <papu.arza@gmail.com>
2017-12-06 20:46:19 +0100

    some bugs fixed

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-const session         = require("express-session");
-const passport        = require("passport");
-const MongoStore   = require('connect-mongo')(session);
-const authController  = require("./routes/auth-controller");
-var index = require('./routes/index');
-var experiences = require('./routes/experiences');
-var academics = require('./routes/academics');
-var skills = require('./routes/skills');
-var extras = require('./routes/extras');
-var curriculums = require('./routes/curriculums');
+require('dotenv').config();
+const express           = require('express');
+const path              = require('path');
+const logger            = require('morgan');
+const cookieParser      = require('cookie-parser');
+const bodyParser        = require('body-parser');
+const session           = require("express-session");
+const passport          = require("passport");
+const MongoStore        = require('connect-mongo')(session);
+const authController    = require("./routes/auth-controller");
+const index             = require('./routes/index');
+const experiences       = require('./routes/experiences');
+const academics         = require('./routes/academics');
+const skills            = require('./routes/skills');
+const extras            = require('./routes/extras');
+const curriculums       = require('./routes/curriculums');
+const cors              = require('cors');
+const mongoose          = require("mongoose");
 
-
-var cors = require('cors');
-
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/passport-local");
+mongoose.connect(process.env.DBURL).then(() =>{
+  console.log(`Connected to DB: ${process.env.DBURL}`);
+});
 
 var app = express();
 
@@ -41,8 +41,6 @@ app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
